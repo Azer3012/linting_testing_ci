7b5f598bd34f45d66e567ab8aa2ae15836578e53
azer <azer@azers-MacBook-Pro.local>
2022-12-17 00:23:14 +0400

    test

--- src/getLength.js
+++ src/getLength.js
@@ -1,17 +1,12 @@
-
-
 export const getLength = (val) => {
-     total = 0;
-    if(!val){
-        return 0
+    let  total = 0;
+    if (!val) {
+        return 0;
     }
     for (let i = 0; i < val.length; i++) {
         total++;
     }
     return total;
 };
-
-
-
 
 console.log(getLength(5));
